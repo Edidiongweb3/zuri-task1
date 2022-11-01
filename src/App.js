565b4ba05f36828cb5bb7f4352826651347e75fb
Edidiongweb3 <okonedidiong1212@gmail.com>
2022-11-01 22:16:21 +0100

    zuri task1

--- src/App.js
+++ src/App.js
@@ -39,15 +39,15 @@ function App() {
       
 
       <div className='icon-area'>
-      <img src={slackimg} className='slack-img'></img>
+      <img src={slackimg} className='slack-img' alt=''></img>
 
-      <img src={gitimg} className='github'></img> 
+      <img src={gitimg} className='github' alt=''></img> 
       </div>
 
         <div className='footer'>      
-        <img src={zuriimg} className='image1' id='image1'></img>
+        <img src={zuriimg} className='image1' alt='' id='image1'></img>
         <p className='text'>HNG Intership 9 Frontend Task</p>
-        <img src={hngicon} className='image' id='image'></img>
+        <img src={hngicon} className='image' alt='' id='image'></img>
         </div>
       
     </div>
